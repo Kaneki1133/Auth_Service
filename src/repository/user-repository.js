d6c28ef2652df0960db43a2fde9395bef6f3a7a0
Kaneki1133 <shloktiwari1133@gmail.com>
2026-06-25 15:07:59 +0530

    Added get user by id in Repository Layer

--- src/repository/user-repository.js
+++ src/repository/user-repository.js
@@ -25,6 +25,18 @@ class UserRepository{
             throw error;
         }
     }
+
+    async getById(userId){
+        try {
+            const user = await User.findByPk(userId , {
+                attributes: [ `email` , `id` ],
+            });
+            return user;
+        } catch (error) {
+            console.log("Something Went Wrong in The Repository layer");
+            throw error;
+        }
+    }
 }
 
 module.exports = UserRepository;
